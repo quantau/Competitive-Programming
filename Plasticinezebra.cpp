95b8857d97a6a9be5b8978bdfcb6812dd480236f
Harshit Singh <geniousharshit@gmail.com>
2021-12-10 10:23:36 +0530

    'solvedZebra'

--- Plasticinezebra.cpp
+++ Plasticinezebra.cpp
@@ -1,12 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define sz(x) x.size()
 #define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(),v.rend()
+#define rall(v) v.rbegin(), v.rend()
 #define nl "\n";
-#define dbg(i) cout<<"HERE "<<i<<endl;
-#define var(x,y,z) cout<<x<<" "<<y<<" "<<z<<endl;
+#define dbg(i) cout << "HERE " << i << endl;
+#define var(x, y, z) cout << x << " " << y << " " << z << endl;
 #define ll long long int
-#define pii pair<ll,ll>
+#define pii pair<ll, ll>
 #define pb push_back
 #define ff first
 #define ss second
@@ -23,5 +23,26 @@ int main()
     //freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
 
+    string s;
+    cin >> s;
+    ll n = s.size();
+    ll res = 0, val = 1, in = 0;
+    s += s;
+    char c = s[0];
+    for (ll i = 1; i < 2 * n; i++)
+    {
+        if (s[i] != c && i < in + n)
+        {
+            val++;
+            c = s[i];
+        }
+        else
+        {
+            res = max(res, val);
+            val = 1;
+            in = i;
+        }
+    }
+    cout << res << nl;
     return 0;
 }
