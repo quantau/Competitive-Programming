aec0ac1f8bf2f4c6555a557299c2276e31b42a56
Harshit Singh <geniousharshit@gmail.com>
2021-12-10 10:10:39 +0530

    'addeadCPP'

--- WilliamtheVigilant.cpp
+++ WilliamtheVigilant.cpp
@@ -1,12 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define sz(x) x.size()
 #define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(),v.rend()
+#define rall(v) v.rbegin(), v.rend()
 #define nl "\n";
-#define dbg(i) cout<<"HERE "<<i<<endl;
-#define var(x,y,z) cout<<x<<" "<<y<<" "<<z<<endl;
+#define dbg(i) cout << "HERE " << i << endl;
+#define var(x, y, z) cout << x << " " << y << " " << z << endl;
 #define ll long long int
-#define pii pair<ll,ll>
+#define pii pair<ll, ll>
 #define pb push_back
 #define ff first
 #define ss second
@@ -23,5 +23,40 @@ int main()
     //freopen("input.txt","r",stdin);
     //freopen("output.txt","w",stdout);
 
+    ll n, q;
+    cin >> n >> q;
+    string s;
+    cin >> s;
+    ll ct = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (s.substr(i, 3) == "abc")
+            ct++;
+    }
+    while (q--)
+    {
+        ll i;
+        char c;
+        cin >> i >> c;
+        i--;
+        for (ll j = i - 2; j <= i; j++)
+        {
+            if (j >= 0 && j + 2 < n)
+            {
+                if (s.substr(j, 3) == "abc")
+                    ct--;
+            }
+        }
+        s[i] = c;
+        for (ll j = i - 2; j <= i; j++)
+        {
+            if (j >= 0 && j + 2 < n)
+            {
+                if (s.substr(j, 3) == "abc")
+                    ct++;
+            }
+        }
+        cout << ct << nl;
+    }
     return 0;
 }
